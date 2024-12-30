4c16a7c68a156f42f324a6b91b9a28344d34a5a3
aosaf-e-c <aosaf@connect.hku.hk>
2024-12-30 14:46:04 +0600

    font fix x4

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,10 +1,8 @@
 import { Inter, Lusitana } from 'next/font/google';
-
-export const inter = Inter({ subsets: ['latin'],
-  display: 'swap',
- });
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ 
 export const lusitana = Lusitana({
-    weight: ['400', '700'],
-    subsets: ['latin'],
-    display: 'swap',
-  })
+  weight: ['400', '700'],
+  subsets: ["latin"],
+});
