242c244dce40fa0e5b1ed97c158f7a3005361976
aosaf-e-c <aosaf@connect.hku.hk>
2025-01-01 14:18:37 +0600

    Implement partial prerendering
    Complete chapter 10

--- nextjs-dashboard/app/dashboard/layout.tsx
+++ nextjs-dashboard/app/dashboard/layout.tsx
@@ -1,4 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import { turborepoTraceAccess } from 'next/dist/build/turborepo-access-trace';
+
+export const experimental_ppr = true;
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
